af1354ae4c5f71472bced09a078e7c00f82d8d9f
Justin Blake <justin@hentzia.com>
2015-02-19 07:28:25 -0500

    Initial release

---
+++ proxyprefix/__init__.py
@@ -0,0 +1,7 @@
+"""WSGI middleware to prefix SCRIPT_NAME with X-Forwarded-Prefix header.
+
+For services behind a reverse proxy so their URLs contain the proxied path.
+
+"""
+
+__version__ = '0.1.0'
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+import proxyprefix
+
+
+setup(
+    name='proxyprefix',
+    version=proxyprefix.__version__,
+    description=proxyprefix.__doc__,
+    url='https://github.com/yola/proxyprefix',
+    packages=['proxyprefix'],
+)
