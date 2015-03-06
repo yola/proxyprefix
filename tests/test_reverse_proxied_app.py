11ac8cde4c194300f154053242520491ae775edc
Justin Blake <justin@hentzia.com>
2015-03-06 05:37:51 -0500

    Move set_scheme tests to their own class
    I'm about to expand on what happens in this function

--- tests/test_reverse_proxied_app.py
+++ tests/test_reverse_proxied_app.py
@@ -1,7 +1,7 @@
 from mock import Mock, patch
 from unittest2 import TestCase
 
-from proxyprefix.wsgi import prefix_paths, ReverseProxiedApp
+from proxyprefix.wsgi import prefix_paths, set_scheme, ReverseProxiedApp
 
 
 class TestReverseProxiedApp(TestCase):
@@ -9,6 +9,10 @@ class TestReverseProxiedApp(TestCase):
         prefix_paths_patcher = patch('proxyprefix.wsgi.prefix_paths')
         self.prefix_paths = prefix_paths_patcher.start()
         self.addCleanup(prefix_paths_patcher.stop)
+
+        set_scheme_patcher = patch('proxyprefix.wsgi.set_scheme')
+        self.set_scheme = set_scheme_patcher.start()
+        self.addCleanup(set_scheme_patcher.stop)
 
         self.environ = {}
         self.start_response = Mock()
@@ -32,12 +36,12 @@ class TestReverseProxiedApp(TestCase):
 
     def test_it_does_not_set_scheme_if_no_HTTP_X_FORWARDED_PROTO(self):
         self.proxied_app(self.environ, self.start_response)
-        self.assertIsNone(self.environ.get('wsgi.url_scheme'))
+        self.assertFalse(self.set_scheme.called)
 
     def test_it_sets_scheme_to_HTTP_X_FORWARDED_PROTO(self):
         self.environ['HTTP_X_FORWARDED_PROTO'] = 'http'
         self.proxied_app(self.environ, self.start_response)
-        self.assertEqual(self.environ.get('wsgi.url_scheme'), 'http')
+        self.set_scheme.assert_called_with(self.environ, 'http')
 
 
 class TestPrefixPaths(TestCase):
@@ -60,3 +64,18 @@ class TestPrefixPaths(TestCase):
         self.environ['SCRIPT_URL'] = '/script/url'
         prefix_paths(self.environ, 'prefix')
         self.assertFalse(self.environ['SCRIPT_URL'])
+
+
+class TestSetScheme(TestCase):
+    """set_scheme(environ, scheme)"""
+
+    def setUp(self):
+        self.environ = {}
+
+    def test_sets_wsgi_url_scheme_to_https_if_scheme_is_https(self):
+        set_scheme(self.environ, 'https')
+        self.assertEqual(self.environ['wsgi.url_scheme'], 'https')
+
+    def test_sets_wsgi_url_scheme_to_http_if_scheme_is_not_https(self):
+        set_scheme(self.environ, 'foo')
+        self.assertEqual(self.environ['wsgi.url_scheme'], 'http')
